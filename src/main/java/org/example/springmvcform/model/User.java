dabf49f9b4c852a3a30c54bc55756d6c1b05eb2b
Konulb13 <konul.museib@gmail.com>
2024-07-01 18:21:22 +0200

    commit2

--- src/main/java/org/example/springmvcform/model/User.java
+++ src/main/java/org/example/springmvcform/model/User.java
@@ -6,7 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString   //  is it necessary?
 @Entity
 public class User {
     @Id
@@ -23,55 +29,55 @@ public class User {
     @Email(message = "Email should be valid")
     private String email;
 
-    public User() {
-    }
-
-    public User(int id, String email, String lastName, String name) {
-        this.id = id;
-        this.email = email;
-        this.lastName = lastName;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName()+
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+//    public User() {
+//    }
+//
+//    public User(int id, String email, String lastName, String name) {
+//        this.id = id;
+//        this.email = email;
+//        this.lastName = lastName;
+//        this.name = name;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return getClass().getSimpleName()+
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", lastName='" + lastName + '\'' +
+//                ", email='" + email + '\'' +
+//                '}';
+//    }
 }
